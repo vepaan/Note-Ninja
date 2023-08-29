c71bc83da72ba9d4f1867f32be4076b61e2e3649
vepaan <141347566+vepaan@users.noreply.github.com>
2023-08-29 19:45:13 +0545

    notesninja edit

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 const dynamicText = document.querySelector("h1 span");
-const words = ["NoteNinja", "Knowledge", "the Future",];
+const words = ["NotesNinja", "Knowledge", "the Future",];
 
 // Variables to track the position and deletion status of the word
 let wordIndex = 0;
