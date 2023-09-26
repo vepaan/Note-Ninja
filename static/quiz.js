dfeac2751ce41e72ed9cd1276d1ffcce20088b4f
vepaan <141347566+vepaan@users.noreply.github.com>
2023-09-26 19:17:16 +0545

    whole din gayo sala

--- static/quiz.js
+++ static/quiz.js
@@ -18,3 +18,15 @@ function start(){
     }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var optionButtons = document.querySelectorAll('.question-box option-new button');
+  
+    optionButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        optionButtons.forEach(function(btn) {
+          btn.classList.remove('active');
+        });
+        this.classList.add('active');
+      });
+    });
+  });
